4790abebf7a4a5ef1d6230dbe5939e7ea62e5355
athum <at3061@barnard.edu>
2016-12-30 23:25:29 -0500

    added methods to place piece onto board, timer to drop pieces

--- tetris.py
+++ tetris.py
@@ -12,6 +12,8 @@ class Game:
 
 	tetrisPieces = []
 	tetrisPieceColors = []
+	isGameOver = False
+	score = 0
 
 	#variables to keep track of falling piece
 	fallingPiece = []
@@ -39,16 +41,16 @@ class Game:
 		#creates new falling piece
 		self.newFallingPiece()
 
-		#self.moveFallingPiece(0, 4)
-		#print self.moveIsLegal(self.pieceX+13, self.pieceY-4)
+		self.drop()
 
+		self.root.bind("<Key>", self.keyPressed)
 		#persists until user exits game
 		self.root.mainloop()
 
 	#clears canvas and draws game
 	def redrawAll(self):
 		#erase existing items
-		self.canvas.delete("all")
+		self.canvas.delete(ALL)
 		self.drawGame()
 
 	#initial method to draw an empty game and board
@@ -132,6 +134,8 @@ class Game:
 
 		#gets starting cells for new piece
 		self.pieceY = self.colNumber/2 - len(self.fallingPiece[0])/2
+		self.pieceX = 0
+		self.redrawAll()
 		self.drawFallingPiece()
 	
 	#draws new falling piece based on boolean values of piece
@@ -142,27 +146,80 @@ class Game:
 				if self.fallingPiece[row][col] == True:
 					self.drawCell(self.pieceY+col, self.pieceX+row, self.fallingPieceColor)
 
-	#moves falling piece if move is legal
+	#moves falling piece if move is legal, redraws and draws falling piece again
 	def moveFallingPiece(self, drow, dcol):
-		if self.moveIsLegal(self.pieceX+drow, self.pieceY+dcol):
+		if self.moveIsLegal(self.fallingPiece, self.pieceX+drow, self.pieceY+dcol):
 			self.pieceX += drow
 			self.pieceY += dcol
 			#redraws moved piece and board
 			self.redrawAll()
 			self.drawFallingPiece()
+			return True
+		return False
 	
 	#checks if move is legal
-	def moveIsLegal(self, x, y):
+	def moveIsLegal(self, piece, x, y):
 		#checks piece at x, y for legal move
-		for row in range(len(self.fallingPiece)):
-				for col in range(len(self.fallingPiece[row])):
-					#checks if move out of range of canvas
-					if x+row not in range(self.rowNumber) or y+col not in range(self.colNumber):
-						return False
-					#checks if board filled at that position
-					if self.fallingPiece[row][col] == True and self.board[x][y] != self.emptyColor:
-						return False
+		for row in range(len(piece)):
+			for col in range(len(piece[row])):
+				#checks if move out of range of canvas
+				if x+row not in range(self.rowNumber) or y+col not in range(self.colNumber):
+					return False
+				#checks if board filled at that position
+				if piece[row][col] == True and self.board[x+row][y+col] != self.emptyColor:
+					return False
 		return True
+
+	def rotateFallingPiece(self):
+		testPiece = zip(*self.fallingPiece[::-1])
+		if self.moveIsLegal(testPiece, self.pieceX, self.pieceY):
+			self.fallingPiece = testPiece
+			self.redrawAll()
+			self.drawFallingPiece()
+
+	def drop(self):
+		delay = 500
+		#if piece cannot move down further, place on board
+		if not self.moveFallingPiece(1, 0):
+			self.placeFallingPiece()
+			self.newFallingPiece()
+			#if new falling piece immediately illegal
+			if not self.moveIsLegal(self.fallingPiece, self.pieceX, self.pieceY):
+				self.isGameOver = True
+				#create replay screen
+				self.gameOver()
+				return
+		self.canvas.after(delay, self.drop)
+
+	#changes board to include piece
+	def placeFallingPiece(self):
+		for row in range(len(self.fallingPiece)):
+			for col in range(len(self.fallingPiece[row])):
+				if self.fallingPiece[row][col] == True:
+					self.board[self.pieceX+row][self.pieceY+col] = self.fallingPieceColor
+		#remove full rows
+
+	#
+	def removeFullRows(self):
+		
+
+	def gameOver(self):
+		pass
+
+	#keyboard commands
+	def keyPressed(self, event):
+		if canvas.data.isGameOver == True:
+			if event.char == "r":
+				canvas.delete(ALL)
+		else:		
+			if event.keysym == 'Left':
+				self.moveFallingPiece(0, -1)
+			elif event.keysym == 'Down':
+				self.moveFallingPiece(1, 0)
+			elif event.keysym == 'Right':
+				self.moveFallingPiece(0, 1)
+			elif event.keysym == 'Up':
+				self.rotateFallingPiece()
 
 game = Game(15, 10)
 
